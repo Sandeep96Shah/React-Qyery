4522b727bc7ff1911630d0d28c4d796668ce0c2e
Sandeep Shah <sandeep2016shah@gmail.com>
2025-06-16 00:02:20 +0600

    started with react-query

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import Todos from "./components/Todo";
+
+function App() {
+
+  return (
+    <div>
+      <Todos />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Todo.jsx
@@ -0,0 +1,38 @@
+import { useQuery } from "@tanstack/react-query";
+
+const API = "https://dummyjson.com/recipes/search?limit=10";
+
+const Todos = () => {
+  const fetchTodoList = async () => {
+    const response = await fetch(API);
+    const data = await response.json();
+    return data;
+  };
+  const { isPending, isError, data, error, isLoading, isFetching } = useQuery({
+    queryKey: ["todos"],
+    queryFn: fetchTodoList,
+    staleTime: Infinity
+  });
+
+  console.log({isLoading, isFetching, isPending});
+
+  if (isPending) {
+    return <span>Loading...</span>;
+  }
+
+  if (isError) {
+    return <span>Error: {error.message}</span>;
+  }
+
+  // We can assume by this point that `isSuccess === true`
+  return (
+    <ul>
+      {/* {data.map((todo) => (
+        <li key={todo.id}>{todo.title}</li>
+      ))} */}
+      {JSON.stringify(data)}
+    </ul>
+  );
+};
+
+export default Todos;
